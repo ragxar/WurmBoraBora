032fc41e43c710fb3649336359bfcbb48a2a306c
Ragxar <51723130+ragxar@users.noreply.github.com>
2026-01-18 18:52:04 +0100

    Use GameHooks and reflections

--- src/main/java/com/github/ragxar/wurm/client/mod/WurmBoraBora.java
+++ src/main/java/com/github/ragxar/wurm/client/mod/WurmBoraBora.java
@@ -1,12 +1,8 @@
 package com.github.ragxar.wurm.client.mod;
 
 import com.github.ragxar.wurm.client.mod.borabora.Strings;
-import com.wurmonline.client.renderer.gui.HeadsUpDisplay;
-import com.wurmonline.client.renderer.gui.MainMenu;
-import com.wurmonline.client.renderer.gui.WurmComponent;
-import com.wurmonline.client.renderer.gui.QuestWizardWindow;
+import com.wurmonline.client.renderer.gui.*;
 import com.wurmonline.client.settings.SavePosManager;
-
 import org.gotti.wurmunlimited.modloader.ReflectionUtil;
 import org.gotti.wurmunlimited.modloader.classhooks.HookManager;
 import org.gotti.wurmunlimited.modloader.interfaces.Configurable;
@@ -14,28 +10,24 @@ import org.gotti.wurmunlimited.modloader.interfaces.Initable;
 import org.gotti.wurmunlimited.modloader.interfaces.PreInitable;
 import org.gotti.wurmunlimited.modloader.interfaces.WurmClientMod;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
-public class WurmBoraBora implements WurmClientMod, Initable, PreInitable, Configurable {
-	public static Logger logger = Logger.getLogger(WurmBoraBora.class.getSimpleName());
-
-	public static boolean handleInput(final String cmd, final String[] data) {
-		return false;
-	}
-
-	@Override
+public class WurmBoraBora extends InjectionTargetsWurmBoraBora implements WurmClientMod, Initable, PreInitable, Configurable {
+	private final static Logger logger = Logger.getLogger(WurmBoraBora.class.getSimpleName());
+    
+    @Override
 	public void init() {
-        HookManager.getInstance().registerHook("com.wurmonline.client.renderer.gui.HeadsUpDisplay", "init", "(II)V", () -> (proxy, method, args) -> {
-            Object invokeResult = method.invoke(proxy, args);
-            registerQuestWizardWindow((HeadsUpDisplay) proxy);
-            return invokeResult;
-        });
-        
-        HookManager.getInstance().registerHook("com.wurmonline.client.renderer.gui.InventoryListComponent", "removeFakeInventoryItem", "(J)V", () -> (proxy, method, args) -> {
+        GameHooks.initialize(this);
+    }
+    
+    @GameHooksMethod(targets = { InventoryListComponent.class })
+    public static void registerCrashFixHooks() {
+        logger.info("register client crash fix hooks");
+        HookManager.getInstance().registerHook(InventoryListComponent.getClassName(), "removeFakeInventoryItem", "(J)V", () -> (proxy, method, args) -> {
             try {
                 return method.invoke(proxy, args);
             } catch (Exception ignore) {
@@ -43,14 +35,14 @@ public class WurmBoraBora implements WurmClientMod, Initable, PreInitable, Confi
             }
         });
         
-        HookManager.getInstance().registerHook("com.wurmonline.client.renderer.gui.InventoryListComponent", "removeInventoryItem", "(Lcom/wurmonline/client/game/inventory/InventoryMetaItem;)V", () -> (proxy, method, args) -> {
+        HookManager.getInstance().registerHook(InventoryListComponent.getClassName(), "removeInventoryItem", "(Lcom/wurmonline/client/game/inventory/InventoryMetaItem;)V", () -> (proxy, method, args) -> {
             try {
                 return method.invoke(proxy, args);
             } catch (Exception ignore) {
                 return null;
             }
         });
-	}
+    }
 
 	@Override
 	public void preInit() {
@@ -58,26 +50,37 @@ public class WurmBoraBora implements WurmClientMod, Initable, PreInitable, Confi
 
     @Override
     public void configure(Properties properties) {
-        String language = properties.getProperty("language");
-        logger.log(Level.INFO, "Configure language as " + language);
+        String language = properties.getProperty("language", "en");
+        logger.info("Configure language as " + language);
         Strings.init(language, getClass().getName());
     }
-
-	private void registerQuestWizardWindow(HeadsUpDisplay hud) {
-		try {
-			QuestWizardWindow questWizardWindow = new QuestWizardWindow();
-
-			MainMenu mainMenu = ReflectionUtil.getPrivateField(hud, ReflectionUtil.getField(hud.getClass(), "mainMenu"));
-			List<WurmComponent> components = ReflectionUtil.getPrivateField(hud, ReflectionUtil.getField(hud.getClass(), "components"));
-			SavePosManager savePosManager = ReflectionUtil.getPrivateField(hud, ReflectionUtil.getField(hud.getClass(), "savePosManager"));
-
-			mainMenu.registerComponent("Bora Quest Wizard", questWizardWindow);
-			mainMenu.setEnabled(questWizardWindow, false);
-			savePosManager.registerAndRefresh(questWizardWindow, "bora-bora-quest");
-
-			logger.log(Level.INFO, "Window registered");
-		} catch (IllegalArgumentException | IllegalAccessException | ClassCastException | NoSuchFieldException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    
+    @GameHooksFinallyTask
+    @SuppressWarnings("unused")
+    public Runnable registerQuestWizardWindow(HeadsUpDisplay hud) {
+        return new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    logger.info("Registering window...");
+                    QuestWizardWindow questWizardWindow = new QuestWizardWindow();
+                    
+                    ReflectionFieldAccessory hudFieldAccessory = new ReflectionFieldAccessory(hud);
+                    MainMenu mainMenu = (MainMenu) hudFieldAccessory.get("mainMenu").orElse(null);
+                    @SuppressWarnings("unchecked")
+                    List<WurmComponent> components = (List<WurmComponent>) hudFieldAccessory.get("components").orElse(new ArrayList<>());
+                    SavePosManager savePosManager = (SavePosManager) hudFieldAccessory.get("savePosManager").orElse(null);
+                    
+                    if (mainMenu == null) return;
+                    mainMenu.registerComponent("Bora Quest Wizard", questWizardWindow);
+                    mainMenu.setEnabled(questWizardWindow, false);
+                    
+                    if (savePosManager == null) return;
+                    savePosManager.registerAndRefresh(questWizardWindow, "bora-bora-quest");
+                } catch (IllegalArgumentException | ClassCastException e) {
+                    logger.severe(e.getMessage());
+                }
+            }
+        };
+    }
 }
